d03c4e743aca52af67a9f896501dddeb864917cb
Yuri Vieira dos Santos <yurivieiradossantos@Yuris-Laptop-2.local>
2025-01-11 22:35:38 -0300

    Insert verification of arguments

---
+++ core_process.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   core_process.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yurivieiradossantos <yurivieiradossanto    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/11 22:01:50 by yurivieirad       #+#    #+#             */
+/*   Updated: 2025/01/11 22:16:30 by yurivieirad      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+int processe_forks(int argc, char **argv)
+{
+	int	fd[2];
+	if (pipe(fd) == -1)
+		return 1;
+	
+	int	pid1 = fork();
+	if (pid1 < 0) 
+		return 2;
+	
+	if(pid1 == 0)
+	{
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		execlp("ls", "ls", "-la", NULL);
+	}
+	int pid2 = fork();
+	if (pid2 < 0 )
+		return 3;
+	if (pid2 == 0)
+	{
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		execlp("more", "more", NULL);
+	}
+	close(fd[0]);
+	close(fd[1]);
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
+	return (0);
+}
--- pipex.c
+++ pipex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yvieira- <yvieira-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yurivieiradossantos <yurivieiradossanto    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 18:24:09 by yvieira-          #+#    #+#             */
-/*   Updated: 2025/01/08 18:24:09 by yvieira-         ###   ########.fr       */
+/*   Updated: 2025/01/11 22:15:27 by yurivieirad      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,32 +18,11 @@
 
 int	main( int argc, char* argv[])
 {
-	int	fd[2];
-	if (pipe(fd) == -1)
-		return 1;
+	if(argc != 5)
+		EXIT_FAILURE;
+	if(!argv[2] || !argv[3])
+		EXIT_FAILURE;
+
+	return processe_forks(argc, argv, **env);
 	
-	int	pid1 = fork();
-	if (pid1 < 0) 
-		return 2;
-	if(pid1 == 0)
-	{
-		dup2(fd[1], STDOUT_FILENO);
-		close(fd[0]);
-		close(fd[1]);
-		execlp("ls", "ls", "-la", NULL);
-	}
-	int pid2 = fork();
-	if (pid2 < 0 )
-		return 3;
-	if (pid2 == 0)
-	{
-		dup2(fd[0], STDIN_FILENO);
-		close(fd[0]);
-		close(fd[1]);
-		execlp("more", "more", NULL);
-	}
-	close(fd[0]);
-	close(fd[1]);
-	waitpid(pid1, NULL, 0);
-	waitpid(pid2, NULL, 0);
 }
